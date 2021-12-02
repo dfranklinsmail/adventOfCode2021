751fdaf32cedd27bc40f60e91befc463c7322056
Daniel Franklin <dfranklinsmail@gmail.com>
2021-12-01 22:16:32 -0500

    part A of day1

---
+++ day1.rs
@@ -0,0 +1,31 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    // File hosts must exist in current path before this produces output
+    let mut count = 0;
+    let mut last = 200;
+    let mut current;
+    if let Ok(lines) = read_lines("day1.dat") {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            if let Ok(ip) = line {
+                current = ip.parse::<i32>().unwrap();
+                if current > last {
+                    count = count +1;
+                }
+                last = current;
+            }
+        }
+        println!("total {}", count);
+    }
+}
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
